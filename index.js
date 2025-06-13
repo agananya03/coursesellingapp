cf55582a379b98a2435fe29e7dab99f965c07a8e
agananya03 <agananya54126@gmail.com>
2025-06-13 13:29:13 +0530

    add

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const jwt = require("jsonwebtoken");
+
+
+
+app.post('/signup' , function(req , res) {
+
+});
+
+app.post('/login' , function(req , res) {
+
+});
+
+app.get('/purchased' , function(req , res) {
+
+});
+
+app.get('/courses' , function(req , res) {
+
+});
+
+app.post('/purchase' , function (req , res) {
+
+});
+
+app.listen(3000);
