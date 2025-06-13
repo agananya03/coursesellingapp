aaaf1028732597b5110a883de42435e11707f932
agananya03 <agananya54126@gmail.com>
2025-06-13 21:50:46 +0530

    add

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require('dotenv').config();
+console.log("connected");
 const { userRouter } = require("./routes/user");
 const { adminRouter } = require("./routes/admin");
 const { coursesRouter } = require("./routes/courses");
 
 const app = express();
-console.log("userRouter:", userRouter);
-console.log("adminRouter:", adminRouter);
-console.log("coursesRouter:", coursesRouter);
 
 app.use("/api/v1/user" , userRouter);
 app.use("/api/v1/admin" , adminRouter);
 app.use("/api/v1/courses" , coursesRouter);
+
+async function main(){
+    await mongoose.connect(process.env.MONGO_URL);
+    app.listen(port, ()=>{
+        console.log("Backend is live!");
+    })
+}
 
 app.listen(3000);
